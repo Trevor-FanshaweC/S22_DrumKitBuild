9da4aa5cbc1f21efd161635872d3d61c4e6dd8e1
Van Rys <tvanrys@ats-hr420-dmpod.fanshawec.ca>
2022-06-17 13:19:02 -0400

    keyboard events are working

--- js/main.js
+++ js/main.js
@@ -2,5 +2,36 @@
 
 (() => {
     console.log('music player script file');
+
+    let myObject = {
+        name: 'foo',
+        role: 'bar'
+    }
+
+    function logKeyboardKeyCode(event) {
+        // event is an object that gets generated whenever the user presses a keyboard key -> it has lots of useful information inside of it (all about the event itself)
+
+        // find the audio element that matches the keyboard keyCode (which one did the user press?)
+
+        // querySelector can take any valid CSS selector, including attributes
+        let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+
+        // the return statement is like an ejector button. kills the code at this point, and nothing else will run
+
+        // the if statement evaluates to true or false; if the condition is true, then the return statement is executed which stops (exits) the function. Nothing else will run below that line.
+
+        // this will kill any errors, and make SURE that there's a matching audio element to play
+
+        // the ! is a NOT logical operator (look on MDN for the definition) -> it means targetAudio is null / doesn't exist
+        if (!targetAudio) { return }
+
+        targetAudio.currentTime = 0;
+        targetAudio.play();
+
+        //debugger;
+    }
+
+    // add some event handling for keyboard events
+    window.addEventListener('keyup', logKeyboardKeyCode);
     
 })();
